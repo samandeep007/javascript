90e05c393c75a9bdd00713d49e3fe05b4f262057
samandeep007 <hacktechnicals@gmail.com>
2024-11-16 12:32:13 -0700

    dataTypes

--- 01_variables/02_datatypes.js
+++ 01_variables/02_datatypes.js
@@ -6,3 +6,21 @@ console.log(3+3); //Semicolon is not mandatory, but you can place it there to te
 
 console.log("Samandeep");
 
+let name = "Samandeep Singh";
+let age = 22;
+let isLoggedIn = false;
+
+// Primitives
+// number --> 2 to the power of 53
+// bigint --> bigger values
+// string --> double or single quotes --> " " or ' ' 
+// boolean --> true/false 
+// null --> value and datatype --> standalone value --> empty value --> not zero
+// undefined --> You didn't assign the value
+// symbol --> unique 
+
+
+// Objects
+console.log(typeof age);
+console.log(typeof null); //null is object
+console.log(typeof undefined);

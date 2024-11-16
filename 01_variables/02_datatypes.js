ca7d866256701113b754e59a50b036c1767cf43b
samandeep007 <hacktechnicals@gmail.com>
2024-11-16 12:26:50 -0700

    data  types

---
+++ 01_variables/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3); We are using Node js, so DOM is not available
+
+console.log(3+3); //Semicolon is not mandatory, but you can place it there to tell the compiler that the statement ends here
+
+console.log("Samandeep");
+

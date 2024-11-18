8bb0354529182c054c762dbb840b180365a07a17
samandeep007 <hacktechnicals@gmail.com>
2024-11-18 13:31:57 -0700

    heap and stack comparison

--- 01_variables/07_Memory.js
+++ 01_variables/07_Memory.js
@@ -5,7 +5,7 @@
 
 let myYoutubeName = "Samandeep Singh";
 
-let anotherName = myYoutubeName;
+let anotherName = myYoutubeName; // You will get a copy of myYoutubeName
 
 console.log(anotherName);
 
@@ -13,3 +13,15 @@ anotherName = "Sandhu saab"; // Value changes only for anotherName, while the my
 console.log(anotherName); 
 
 console.log(myYoutubeName); // The value remains the same
+
+let user = {
+    name: "Samandeep Singh",
+    upiId: "samandeep@paytm",
+    address: "164 Taradale Drive"
+};
+
+let userTwo = user; // userTwo points to the same memory location as the user 
+
+userTwo.name = "Tarandeep Kaur";
+
+console.log(user);
